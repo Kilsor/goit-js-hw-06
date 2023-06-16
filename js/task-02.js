38315d6683d63db173c9017a868b77aa0935a719
Kilsor <kilsors@gmail.com>
2023-06-16 17:51:02 +0300

    hw6.1

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients"); //Знаходимо елемент за ID
+
+const items = ingredients.map((name) => {
+  //перебираємо масив інгредієнтів і створюємо масив лішок
+  const li = document.createElement("li"); //створюємо лішку
+  li.classList.add("item"); //додаємо клас
+  li.textContent = name; //додаємо текстовий контент
+  return li; // повертаємо готову лішку
+});
+
+list.append(...items); //поміщаємо список наших елементів в DOM, розпиливши масив лішок. Тому, що метод append приймає все в якості списку
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,29 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const list = document.querySelector(".gallery"); //Знаходимо елемент за класом
+const markup = images //перебираємо масив та деструктуризуємо потрібні властивості
+  .map(
+    //створюємо приклад шаблонної розмітки в HTML (зручно прописати в самому HTML та скопіпастити в JS)
+    ({ url, alt }) => `
+    <li class="gallery-item">
+    <img src="${url}" alt="${alt}" class="gallery-image">
+  </li>
+`
+  )
+  .join(""); //перетворюємо масив в рядок
+
+list.insertAdjacentHTML("beforeend", markup); //додаємо в контейнер розмітку
+console.log(markup);
